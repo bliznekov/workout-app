3986037ca4dd8091259b9c70b3f027ce3520e5c2
bliznekov <r.bliznekov@gmail.com>
2022-06-25 17:27:39 +0300

    feat: added exercises

--- back/server.js
+++ back/server.js
@@ -8,6 +8,7 @@ import { connectDB } from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 // Routes
 import userRoutes from "./routes/userRoutes.js";
+import exerciseRoutes from "./routes/exerciseRoutes.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/exercises", exerciseRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
