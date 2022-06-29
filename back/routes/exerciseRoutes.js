bb1e7ff7c209e95653ed2597e7d5528040eb2b78
bliznekov <r.bliznekov@gmail.com>
2022-06-29 20:50:53 +0300

    feat: init updateController

--- back/routes/exerciseRoutes.js
+++ back/routes/exerciseRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { createNewExerciseLog, getExerciseLog } from "../controllers/exercise/logController.js";
+import { getExerciseLog } from "../controllers/exercise/log/getController.js";
+import { createNewExerciseLog } from "../controllers/exercise/log/logController.js";
+import { updateExerciseLog } from "../controllers/exercise/log/updateController.js";
 import { createNewExercise } from "../controllers/exercise/mainController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, createNewExercise);
-router.route("/log").post(protect, createNewExerciseLog);
+router.route("/log").post(protect, createNewExerciseLog).put(protect, updateExerciseLog);
 router.route("/log/:id").get(protect, getExerciseLog);
 
 export default router;
