57343a1ee57fa58723467ec23684e69f26beb6af
bliznekov <r.bliznekov@gmail.com>
2022-06-29 21:01:39 +0300

    feat: init completed exercise

--- back/routes/exerciseRoutes.js
+++ back/routes/exerciseRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getExerciseLog } from "../controllers/exercise/log/getController.js";
 import { createNewExerciseLog } from "../controllers/exercise/log/logController.js";
-import { updateExerciseLog } from "../controllers/exercise/log/updateController.js";
+import { updateCompleteExerciseLog, updateExerciseLog } from "../controllers/exercise/log/updateController.js";
 import { createNewExercise } from "../controllers/exercise/mainController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.route("/").post(protect, createNewExercise);
 router.route("/log").post(protect, createNewExerciseLog).put(protect, updateExerciseLog);
+router.route("/log/completed").put(protect, updateCompleteExerciseLog);
 router.route("/log/:id").get(protect, getExerciseLog);
 
 export default router;
